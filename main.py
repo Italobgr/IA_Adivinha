c0fc3f3c6ba876fcd7967200099aafb72ab27340
Italo Brunno <italo.leite@mgs.srv.br>
2025-07-16 16:57:31 -0300

    basic concepts

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('Hello World new max');
+
+
+
+
+
+
+fruits = ["apple", "banana", "cherry"]
+
+for x in range(5):
+    print('1'); 
+
+
+
+
+
+
+
+
+
+
+#Precedencia
+#() sempre e executado de dentro para fora, os inter
+#**
+# *  /  //  %
+#conta_1 = 1 + 1 ** 5 + 5  # resultado e 7 pois 1 ** 5 conta separado e resultado e 1
+
+
+
+#concatenação
+#concatenacao = 'A' + 'B' + 'C'
+
+#a_dez_vezes = 'A' * 10 
+
+
+#tipagem
+#print(int('1'), type(int('1'))); str()
+
