f3e54dd25e454762ec4a911a954fb2254fb2a23c
Italobgr <italobrunobgr@gmail.com>
2025-07-17 23:00:45 -0300

    execise to calc

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 
+#exeecicio calculo imc
+#nome = 'italo Brunno de Sousa';
+#Altura = 1.80;
+#Peso =  95;
+
+#imc = Peso / (Altura ** 2);
+#print(imc);
 
 
+#listas
+#fruits = ["apple", "banana", "cherry"]
 
-
-
-fruits = ["apple", "banana", "cherry"]
-
-for x in range(5):
-    print('1'); 
-
-
-
-
-
-
-
-
-
+#lacos
+#for x in range(5):
+    #print('1'); 
 
 #Precedencia
 #() sempre e executado de dentro para fora, os inter
@@ -24,13 +21,10 @@ for x in range(5):
 # *  /  //  %
 #conta_1 = 1 + 1 ** 5 + 5  # resultado e 7 pois 1 ** 5 conta separado e resultado e 1
 
-
-
 #concatenação
 #concatenacao = 'A' + 'B' + 'C'
 
 #a_dez_vezes = 'A' * 10 
-
 
 #tipagem
 #print(int('1'), type(int('1'))); str()
