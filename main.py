146060e280b9888bf94aa5b9d39b0efde15eb530
Italobgr <italobrunobgr@gmail.com>
2025-07-22 21:25:46 -0300

    feat: study about format

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
-#exeecicio calculo imc
-#nome = 'italo Brunno de Sousa';
-#Altura = 1.80;
-#Peso =  95;
+#exercicio calculo imc
+nome = 'italo Brunno de Sousa';
+Altura = 1.80;
+Peso =  95;
 
-#imc = Peso / (Altura ** 2);
+imc = Peso / (Altura ** 2);
 #print(imc);
 
 
@@ -19,7 +19,7 @@
 #() sempre e executado de dentro para fora, os inter
 #**
 # *  /  //  %
-#conta_1 = 1 + 1 ** 5 + 5  # resultado e 7 pois 1 ** 5 conta separado e resultado e 1
+conta_1 = 1 + 1 ** 5 + 5  # resultado e 7 pois 1 ** 5 conta separado e resultado e 1
 
 #concatenação
 #concatenacao = 'A' + 'B' + 'C'
@@ -29,3 +29,19 @@
 #tipagem
 #print(int('1'), type(int('1'))); str()
 
+#FORMATAÇÃO COM F STRING
+
+print(f'E o resultado da conta e {conta_1}');#:.2f para casas decimais/ umas virgula antes do ponto e para monetarios
+print(f'seu IMC e {imc:.2f}')
+
+#FORMATAÇÃO COM O FORMAT() -- terminar de ver os conceitos
+
+a = '';
+b = '';
+c = 1.1;
+
+string = 'a={nome1}';
+
+formato = string.format(nome1=a);
+
+print(formato);
